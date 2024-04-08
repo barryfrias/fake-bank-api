eb3b1b5b66942ed89066102473868f3e55daa005
Barry Richardson Frias <barry.frias@yahoo.com.ph>
2024-04-08 18:14:53 +0800

    committed missing important files

---
+++ src/test/java/org/blfrias/fakebank/FakeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blfrias.fakebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
